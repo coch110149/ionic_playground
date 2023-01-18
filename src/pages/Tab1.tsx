5d6d60697c1a05e50fb8c5f92fd347fb21a418b8
clint <clinton.edward.cochrane@gmail.com>
2023-01-17 22:16:19 -0600

    working colourful scrolling list

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonContent, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonList } from "@ionic/react";
+import { useState, useEffect } from "react";
+
 import './Tab1.css';
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+function ShoppingList() {
+  const [items, setItems] = useState<String[]>([]);
 
-export default Tab1;
+  const generateItems = () => {
+    const newItems = [];
+    for (let i = 0; i < 50; i++) {
+      newItems.push(`Item ${items.length + i}`
+
+      );
+    }
+    setItems([...items, ...newItems]);
+  };
+
+  useEffect(() => {
+    generateItems();
+  }, []);
+
+  const getAColour = () => {
+    let redValue = parseInt(Math.floor(Math.random() * 255).toString(), 16);
+    let blueValue = parseInt(Math.floor(Math.random() * 255).toString(), 16);
+    let greenValue = parseInt(Math.floor(Math.random() * 255).toString(), 16);
+    return redValue + blueValue + greenValue + '';
+  }
+  
+  
+
+  return (
+    <IonContent>
+    <IonList>
+      {items.map((item, index) =>
+      (
+        <IonItem color={'dark'} key={index}>
+          <IonCard style={{ backgroundColor: `#${getAColour()}` , width: '100%', height: '100%' }}>
+         
+            <IonCardContent ></IonCardContent>
+          </IonCard>
+        </IonItem>
+      ))}
+    </IonList>
+
+    <IonInfiniteScroll onIonInfinite={(ev) => { generateItems(); setTimeout(() => ev.target.complete(), 500); }}>
+      <IonInfiniteScrollContent loadingText="Lemme Get That..." loadingSpinner="bubbles"></IonInfiniteScrollContent>
+
+    </IonInfiniteScroll>
+  </IonContent>
+)}
+export default ShoppingList;
